790e2fd8dbe9ef7e51470c872e308561648b4e5d
MagdalenaVasylyshyn <141566623+MagdalenaVasylyshyn@users.noreply.github.com>
2023-11-03 19:13:52 +0200

    Third version of fourth homework

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,60 @@
 function calcAverageCalories(days) {
-  let totalRating = 0;
-  // let caloriesValues = Object.values(calories);
-  for (const day of days) {
-    totalRating += day.calories;
-    console.log(day);
-  }
-  return totalRating / day.calories.length;
-}
+  if (days.length === 0) {
+    return 0;
+  };
 
-calcAverageCalories([
-  { day: 'monday', calories: 3010 },
-  { day: 'tuesday', calories: 3200 },
-  { day: 'wednesday', calories: 3120 },
-  { day: 'thursday', calories: 2900 },
-  { day: 'friday', calories: 3450 },
-  { day: 'saturday', calories: 3280 },
-  { day: 'sunday', calories: 3300 },
-]);
+  let totalCalories = 0;
+
+  for (const day of days) {
+    totalCalories += day.calories;
+  };
+
+  const averageCalories = totalCalories / days.length;
+
+  return averageCalories;
+};
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
+// calcAverageCalories([
+//   { day: 'monday', calories: 3010 },
+//   { day: 'tuesday', calories: 3200 },
+//   { day: 'wednesday', calories: 3120 },
+//   { day: 'thursday', calories: 2900 },
+//   { day: 'friday', calories: 3450 },
+//   { day: 'saturday', calories: 3280 },
+//   { day: 'sunday', calories: 3300 },
+// ]);
+
+  // let totalRating = 0;
+  // // let caloriesValues = Object.values(calories);
+  // for (const day of days) {
+  //   totalRating += day.calories;
+  //   console.log(day);
+  // }
+  // return totalRating / day.calories.length;
--- js/task-3.js
+++ js/task-3.js
@@ -2,18 +2,30 @@ const profile = {
   username: 'Jacob',
   playTime: 300,
   changeUsername(newName) {
-    for (const usernam of this.username) {
-      if (this.usernam === this.username) {
-        this.usernam = newName;
-      }
-    }
+    for (const profil in profile) {
+      if (this.username === this.username) {
+        this.username = newName;
+      };
+    };
   },
   updatePlayTime(hours) {
-    for (const usernam of this.username) {
-      if (this.usernam === this.username) {
-        this.usernam = newName;
-      }
-    }
+    for (const profil in profile) {
+      if (this.playTime === this.playTime) {
+        this.playTime += hours;
+      };
+    };
   },
-  getInfo() {},
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
 };
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
